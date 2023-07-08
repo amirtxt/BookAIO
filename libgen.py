2db40387cb941b388d27ebce7f0aa8cfd0d7e4c8
amirtxt <amir.tahanan2004@gmail.com>
2023-07-07 23:29:17 -0500

    added headless mode (not working))

--- libgen.py
+++ libgen.py
@@ -2,12 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import easygui
 
 PATH = (r"C:\Users\Amir\Documents\Downloads\chromedriver.exe")
 driver = webdriver.Chrome(PATH)
+
+options = Options()
+options.add_argument('--headless=new')
+driver = webdriver.Chrome(r"C:\Users\Amir\Documents\Downloads\chromedriver.exe", options=options)
 
 def slibgen():
     driver.get("https://libgen.is/")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from libgen import slibgen
@@ -10,6 +11,10 @@ import easygui
 PATH = (r"C:\Users\Amir\Documents\Downloads\chromedriver.exe")
 driver = webdriver.Chrome(PATH)
 driver.quit
+
+options = Options()
+options.add_argument('--headless=new')
+driver = webdriver.Chrome(r"C:\Users\Amir\Documents\Downloads\chromedriver.exe", options=options)
 
 type = easygui.choicebox("","BookAIO",["Book","Paper"])
 
